f0f101489ff55712790e73ce9c5f248b3cfd8dfa
huang2a <huang2a>
2016-05-31 18:05:39 +0800

    update  submenu
    更新submenu 菜单展开问题的id序号问题！

--- js/custom.js
+++ js/custom.js
@@ -1,111 +1,1 @@
-$(document).ready(function(){
-
-
-	$('.deploy-contact-form').click(function(){	$('.sidebar-form').toggle(300);});
-	$('.deploy-subscribe-form').click(function(){$('.sidebar-form2').toggle(300); $('#emailError').hide(200);});
-	
-	
-	$('#submenu-one').click(function(){	$('.submenu-one').toggle(300);	});
-	$('#submenu-two').click(function(){	$('.submenu-two').toggle(300);	});
-	$('#submenu-three').click(function(){	$('.submenu-three').toggle(300);	});
-	$('#submenu-four').click(function(){	$('.submenu-four').toggle(300);	});
-	$('#submenu-five').click(function(){	$('.submenu-five').toggle(300);	});
-	$('#submenu-six').click(function(){	$('.submenu-six').toggle(300);	});
-	$('#submenu-seven').click(function(){	$('.submenu-seven').toggle(300);	});
-	$('#submenu-eight').click(function(){	$('.submenu-eight').toggle(300);	});
-	
-	$('.deploy-left-sidebar, .close-sidebar-left, .close-bottom-left, .close-bottom-right, .deploy-subscribe-form, .deploy-contact-form').click(function(){	return false;	})
-	
-
-	///////////////////////
-	//Deploy Left Sidebar//
-	///////////////////////
-    $(".content").click(function(){
-        $('.sidebar-left').animate({
-            left: '-270',
-        }, 300, 'easeOutExpo', function () {});
-        $('.sidebar-right').animate({
-            right: '-280px',
-        }, 300, 'easeInOutExpo', function () {});
-    });
-	
-
-
-	
-	///////////////////////
-	//Deploy Left Sidebar//
-	///////////////////////
-    $(".deploy-left-sidebar").click(function(){
-        $('.sidebar-left').delay(300).animate({
-            left: '0',
-        }, 300, 'easeOutExpo', function () {});
-        $('.sidebar-right').animate({
-            right: '-280px',
-        }, 300, 'easeInOutExpo', function () {});
-        return false;
-    });
-	
-	
-	//////////////////////
-	//Close Left Sidebar//
-	//////////////////////
-
-    $(".close-sidebar-left, .close-bottom-left").click(function(){        
-        $('.sidebar-left').animate({
-            left: '-270px',
-        }, 300, 'easeInOutExpo', function () {});
-        return false;
-    });
-
-
-	////////////////////////
-	//Deploy Right Sidebar//
-	////////////////////////
-
-    $('.deploy-right-sidebar').click(function () {
-        $('.sidebar-right').delay(300).animate({
-            right: '-10',
-        }, 300, 'easeOutExpo', function () {});
-        $('.sidebar-left').animate({
-            left: '-270px',
-        }, 300, 'easeInOutExpo', function () {});
-        return false;
-    });
-	
-	///////////////////////
-	//Close Right Sidebar//
-	///////////////////////
-
-    $('.close-sidebar-right, .close-bottom-right').click(function () {
-        $('.sidebar-right').animate({
-            right: '-280px',
-        }, 300, 'easeInOutExpo', function () {});
-        return false;
-    });
-
-	$('.deploy-subscribe').click(function(){
-		$('.sidebar-form').hide(200);
-		$('body,html').animate({scrollTop:0},500);
-        $('.sidebar-right').delay(500).animate({
-            right: '-10',
-        }, 300, 'easeOutExpo', function () {});		
-		$('.sidebar-scroll-right').delay(1000).animate({scrollTop:0},500);
-		$('.sidebar-form2').delay(1000).show(200);
-		$('#emailError').delay(1000).show();
-		return false;
-	});
-
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
+$(document).ready(function(){	$('.deploy-contact-form').click(function(){	$('.sidebar-form').toggle(300);});	$('.deploy-subscribe-form').click(function(){$('.sidebar-form2').toggle(300); $('#emailError').hide(200);});    $('#submenu0').click(function(){    $('.submenu0').toggle(300); });	$('#submenu1').click(function(){	$('.submenu1').toggle(300);	});	$('#submenu2').click(function(){	$('.submenu2').toggle(300);	});	$('#submenu3').click(function(){	$('.submenu3').toggle(300);	});	$('#submenu4').click(function(){	$('.submenu4').toggle(300);	});	$('#submenu5').click(function(){	$('.submenu5').toggle(300);	});	$('#submenu6').click(function(){	$('.submenu6').toggle(300);	});	$('#submenu7').click(function(){	$('.submenu7').toggle(300);	});	$('#submenu8').click(function(){	$('.submenu8').toggle(300);	});	$('.deploy-left-sidebar, .close-sidebar-left, .close-bottom-left, .close-bottom-right, .deploy-subscribe-form, .deploy-contact-form').click(function(){	return false;	})	///////////////////////	//Deploy Left Sidebar//	///////////////////////    $(".content").click(function(){        $('.sidebar-left').animate({            left: '-270',        }, 300, 'easeOutExpo', function () {});        $('.sidebar-right').animate({            right: '-280px',        }, 300, 'easeInOutExpo', function () {});    });	///////////////////////	//Deploy Left Sidebar//	///////////////////////    $(".deploy-left-sidebar").click(function(){        $('.sidebar-left').delay(300).animate({            left: '0',        }, 300, 'easeOutExpo', function () {});        $('.sidebar-right').animate({            right: '-280px',        }, 300, 'easeInOutExpo', function () {});        return false;    });	//////////////////////	//Close Left Sidebar//	//////////////////////    $(".close-sidebar-left, .close-bottom-left").click(function(){        $('.sidebar-left').animate({           left: '-270px',        }, 300, 'easeInOutExpo', function () {});        return false;    });	////////////////////////	//Deploy Right Sidebar//	////////////////////////    $('.deploy-right-sidebar').click(function () {        $('.sidebar-right').delay(300).animate({            right: '-10',        }, 300, 'easeOutExpo', function () {});        $('.sidebar-left').animate({           left: '-270px',        }, 300, 'easeInOutExpo', function () {});        return false;    });	///////////////////////	//Close Right Sidebar//	///////////////////////    $('.close-sidebar-right, .close-bottom-right').click(function () {        $('.sidebar-right').animate({            right: '-280px',       }, 300, 'easeInOutExpo', function () {});        return false;    });	$('.deploy-subscribe').click(function(){		$('.sidebar-form').hide(200);		$('body,html').animate({scrollTop:0},500);        $('.sidebar-right').delay(500).animate({            right: '-10',        }, 300, 'easeOutExpo', function () {});		$('.sidebar-scroll-right').delay(1000).animate({scrollTop:0},500);		$('.sidebar-form2').delay(1000).show(200);		$('#emailError').delay(1000).show();		return false;	});});
